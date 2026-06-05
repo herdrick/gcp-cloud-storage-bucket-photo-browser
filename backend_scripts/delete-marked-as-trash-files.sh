09872fde66ef8a88a7b1f950158fbd810ec90a23
Ethan Herdrick <herdrick@gmail.com>
2026-06-04 16:28:17 -1000

    improve delete-marked-as-trash-files.sh

--- backend_scripts/delete-marked-as-trash-files.sh
+++ backend_scripts/delete-marked-as-trash-files.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 BUCKET=$1
-echo "cat this file for deleting 'marked as trash' files"
-# gcloud storage ls gs://$BUCKET/__marked_files/to-trash* | parallel -k "gcloud storage cat {} | grep 'gs://' | parallel --replace [] -j1 'gcloud storage rm [] && gcloud storage rm {}'"
+
+echo "looking for files like gs://$BUCKET/__marked_files/to-trash ..."
+gcloud storage ls gs://$BUCKET/__marked_files/to-trash* | parallel --line-buffer --keep-order --jobs 1 "gcloud storage cat {} | grep 'gs://' | parallel --line-buffer --keep-order --jobs 1 --replace [] 'gcloud storage rm [] ; gcloud storage rm {}'"
